34993796e2a87b53cc1dd17132c698e5ebb05a4f
ylyhappy <2778335106@qq.com>
2023-03-09 15:19:55 +0800

    使用leap跳转

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -101,6 +101,10 @@ return packer.startup(function(use)
    -- deprecated sql lsp write by go
   use 'nanotee/sqls.nvim'
 
+  -- 跳转插件
+  require('leap').add_default_mappings()
+  use 'ggandor/leap.nvim'
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
